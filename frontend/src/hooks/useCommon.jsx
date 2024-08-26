858f206cb7fd93e8a1a5b0da4c245047f4d2a0d5
Aizaz Ahmed <108420885+Aizaz6198@users.noreply.github.com>
2024-08-26 09:51:34 +0530

    Update useCommon.jsx

--- frontend/src/hooks/useCommon.jsx
+++ frontend/src/hooks/useCommon.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosCommon = axios.create({
-    baseURL: "https://full-stack-sales-overview-app.vercel.app/api"
+    baseURL: "https://fullstacksalesoverviewapp.onrender.com/api"
 })
 
 
