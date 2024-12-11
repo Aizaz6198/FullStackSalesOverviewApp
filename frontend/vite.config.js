88ac9d8e0c6945532c67401a7cca617ea527b68f
aizaz <aizaz060198@gmail.com>
2024-12-11 11:25:14 +0530

    git commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: process.env.PORT || 3000, // Use the PORT environment variable or fallback to 3000
+    host: true, // Bind to all available network interfaces
+  },
   plugins: [react()],
 })
